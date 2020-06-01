175b8e8b50a46d42a48a2b7db57a5c44b2f695c9
etamura07 <emerson.tamura07@gmail.com>
2020-06-01 14:16:19 -0300

    Implementing all principal verbs

---
+++ RestApiAspNetCore-Calculator - Using Diferent Verbs/RestApiAspNetCore/Model/Person.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Remotion.Linq.Parsing.Structure.IntermediateModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApiAspNetCore.Model
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+    }
+}
